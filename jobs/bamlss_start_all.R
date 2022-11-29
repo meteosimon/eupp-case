33efc5dc58214648290793d89e00c6c76e1fa0c1
Reto Stauffer <c4031021@login.ib.leo4.intra.uibk.ac.at>
2022-11-29 10:22:41 +0100

    feat: start_all now only starts jobs where the number of results files (rds files) is less than 21

--- jobs/bamlss_start_all.R
+++ jobs/bamlss_start_all.R
@@ -8,14 +8,21 @@ csvfiles <- csvfiles[grepl("euppens_t2m_[a-z]+_[0-9]+_[a-z]+_[0-9]{3}\\.csv", cs
 
 # Extracting country and station ID
 data <- data.frame(country = regmatches(csvfiles, regexpr("[a-z]+(?=_[0-9])", csvfiles, perl = TRUE)),
-                   station = as.integer(regmatches(csvfiles, regexpr("(?!=f_)[0-9]+(?=_)", csvfiles, perl = TRUE))))
+                   station = as.integer(regmatches(csvfiles, regexpr("(?!=f_)[0-9]+(?=_)", csvfiles, perl = TRUE))),
+		   nrds = NA)
+
 # Take unique country/station ID
 data <- unique(data)
+
+all_files <- list.files("../results/bamlss/", recursive = TRUE)
+for (i in seq_len(NROW(data)))
+    data$nrds[i] <- sum(grepl(sprintf("_%s_%d_", data$country[i], data$station[i]), all_files))
 
 print(data)
 
 # Start the job (array jobs)
 for (i in seq_len(nrow(data))) {
+    if (data$nrds[i] == 21) next
     #print(sprintf("sbatch bamlss_run.R -c %s -s %d", data$country[i], data$station[i]))
     system(sprintf("sbatch bamlss_run.R -c %s -s %d", data$country[i], data$station[i]))
 }
