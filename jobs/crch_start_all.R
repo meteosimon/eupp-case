add0738503567ba37a29512483b7c36de04d9824
Reto Stauffer <c4031021@login.ib.leo4.intra.uibk.ac.at>
2022-11-29 13:00:33 +0100

    feat: adding crch_start script

---
+++ jobs/crch_start_all.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+# Find all suitable CSV files
+stopifnot(dir.exists("../euppens"))
+csvfiles <- list.files("../euppens")
+csvfiles <- csvfiles[grepl("euppens_t2m_[a-z]+_[0-9]+_[a-z]+_[0-9]{3}\\.csv", csvfiles)]
+
+# Extracting country and station ID
+data <- data.frame(country = regmatches(csvfiles, regexpr("[a-z]+(?=_[0-9])", csvfiles, perl = TRUE)),
+                   station = as.integer(regmatches(csvfiles, regexpr("(?!=f_)[0-9]+(?=_)", csvfiles, perl = TRUE))))
+# Take unique country/station ID
+data <- unique(data)
+print(data)
+
+# Start the job (array jobs)
+for (i in seq_len(nrow(data))) {
+    cmd <- sprintf("sbatch crch_run.R -c %s -s %d", data$country[i], data$station[i])
+    cat("Calling:        ", cmd, "\n")
+    system(cmd)
+}
+
